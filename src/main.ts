4a7c174a0200a937bbe9959c7788dbeed6b1cdab
Ben Cooley <bcooley@conductor.com>
2019-11-14 19:44:59 -0500

    testys

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,12 @@ async function run() {
   const ALLOWABLE_DEFECTS = Number(core.getInput('DEFECTS'));
   const LABEL = core.getInput('LABEL');
 
+  core.info(
+    REQUIRED_APPROVALS,
+    ALLOWABLE_DEFECTS,
+    LABEL
+  )
+
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set github_token with the GitHub Secret Token
   // github_token: ${{ secrets.GITHUB_TOKEN }}
