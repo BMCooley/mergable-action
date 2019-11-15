85aa9595124dcc03971f0c7004967127bacd0db7
Ben Cooley <bcooley@conductor.com>
2019-11-14 19:40:46 -0500

    use CONSTANT_CASE for input vars

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ async function run() {
   let defects = 0;
   let awaitingResponse = 0;
 
-  const REQUIRED_APPROVALS = Number(core.getInput('approvals'));
-  const ALLOWABLE_DEFECTS = Number(core.getInput('defects'));
-  const LABEL = core.getInput('label');
+  const REQUIRED_APPROVALS = Number(core.getInput('APPROVALS'));
+  const ALLOWABLE_DEFECTS = Number(core.getInput('DEFECTS'));
+  const LABEL = core.getInput('LABEL');
 
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set github_token with the GitHub Secret Token
   // github_token: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret
-  const GITHUB_TOKEN = core.getInput('github_token');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const octokit = new github.GitHub(GITHUB_TOKEN);
 
   const PR_CONFIG = {
